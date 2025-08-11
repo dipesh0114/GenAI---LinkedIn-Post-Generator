66ce430b30acf433e0f23d5ef4737e60a0be8813
Dipesh <dipeshpatro@gmail.com>
2025-08-12 02:24:02 +0530

    as

--- post_generator.py
+++ post_generator.py
@@ -1,4 +1,4 @@
-import llm
+from llm_helper import llm
 from few_shot import FewShotPosts
 
 
--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,5 @@
 import json
-import llm
+from llm_helper import llm
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import JsonOutputParser
 from langchain_core.exceptions import OutputParserException
